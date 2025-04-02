d30d0b579315a7b5855ee30b1d7333d84cdc192a
Vladimir Balin <krocos@icloud.com>
2025-04-02 21:51:37 +0500

    WIP

--- reacto.go
+++ reacto.go
@@ -1,15 +1,16 @@
 package reacto
 
 import (
+	"strings"
 	"sync"
-	"unsafe"
 )
 
 type effect func()
 
-var activeEffect effect
-
-// ---------------------------------------------------------------------------- //
+var (
+	activeToken  string
+	activeEffect effect
+)
 
 type ValueRef[T any] struct {
 	effects *effects
@@ -24,50 +25,47 @@ func Ref[T any](value T) *ValueRef[T] {
 }
 
 func (r *ValueRef[T]) Value() T {
-	r.effects.add(activeEffect)
+	r.effects.add()
 	return r.value
 }
 
 func (r *ValueRef[T]) Set(value T) {
+	if hasActiveEffect() {
+		panic("using Set() method on Ref[T] in Watch() function")
+	}
+
 	r.value = value
 	r.effects.notify()
 }
 
-// ---------------------------------------------------------------------------- //
-
 var mu sync.Mutex
 
-func Watch(e effect) {
+func Watch(token string, e effect) {
 	mu.Lock()
-	defer mu.Unlock()
+	defer func() {
+		activeToken = ""
+		activeEffect = nil
+		mu.Unlock()
+	}()
 
+	activeToken = token
 	activeEffect = e
 	activeEffect()
-	activeEffect = nil
 }
 
-// ---------------------------------------------------------------------------- //
-// Вычисляемая переменная. При создании принимает функцию, которая вычисляет значение.
-
 type ComputedRef[T any] struct {
-	//effects *effects
 	compute func() T
 }
 
 func Computed[T any](compute func() T) *ComputedRef[T] {
 	return &ComputedRef[T]{
-		//effects: newEffects(),
 		compute: compute,
 	}
 }
 
 func (c *ComputedRef[T]) Value() T {
-	//c.effects.add(activeEffect)
 	return c.compute()
 }
-
-// ---------------------------------------------------------------------------- //
-// Набор эффектов для выполнения и добавления.
 
 type effects struct {
 	subscribers sync.Map
@@ -77,13 +75,12 @@ func newEffects() *effects {
 	return &effects{subscribers: sync.Map{}}
 }
 
-func (e *effects) add(ef effect) {
-	if ef == nil {
+func (e *effects) add() {
+	if !hasActiveEffect() {
 		return
 	}
 
-	key := uintptr(unsafe.Pointer(&ef))
-	e.subscribers.Store(key, activeEffect)
+	e.subscribers.Store(activeToken, activeEffect)
 }
 
 func (e *effects) notify() {
@@ -91,4 +88,8 @@ func (e *effects) notify() {
 		ef.(effect)()
 		return true
 	})
+}
+
+func hasActiveEffect() bool {
+	return strings.TrimSpace(activeToken) != "" && activeEffect != nil
 }
--- reacto_test.go
+++ reacto_test.go
@@ -14,7 +14,7 @@ func TestReacto(t *testing.T) {
 		return price.Value() * quantity.Value()
 	})
 
-	reacto.Watch(func() {
+	reacto.Watch("token", func() {
 		t.Log("revenue:", revenue.Value())
 	})
 
@@ -24,4 +24,118 @@ func TestReacto(t *testing.T) {
 	if revenue.Value() != 9 {
 		t.Fatal("unexpected result")
 	}
+}
+
+func TestCorrectReactions(t *testing.T) {
+	a := reacto.Ref(1)
+	b := reacto.Ref(1)
+	c := reacto.Ref(1)
+
+	sumAB := reacto.Computed(func() int { // 2
+		return a.Value() + b.Value()
+	})
+
+	sumAC := reacto.Computed(func() int { // 2
+		return a.Value() + c.Value()
+	})
+
+	reacto.Watch("here we watch for something", func() {
+		v := sumAB.Value()
+		t.Log("a + b:", v)
+	})
+
+	reacto.Watch("here we log something", func() {
+		v := sumAC.Value()
+		t.Log("a + c:", v)
+	})
+
+	a.Set(2) // ab == 3 ac == 3
+	if sumAB.Value() != 3 {
+		t.Error("unexpected result")
+	}
+	if sumAC.Value() != 3 {
+		t.Error("unexpected result")
+	}
+	a.Set(3) // ab == 4 ac == 4
+	if sumAB.Value() != 4 {
+		t.Error("unexpected result")
+	}
+	if sumAC.Value() != 4 {
+		t.Error("unexpected result")
+	}
+}
+
+type User struct {
+	Name  string
+	Age   int
+	Phone string
+}
+
+type Card struct {
+	Number *reacto.ValueRef[string]
+	Pin    string
+}
+
+type State struct {
+	User      *reacto.ValueRef[*User]
+	Card      *reacto.ValueRef[*Card]
+	CardTitle *reacto.ComputedRef[string]
+}
+
+func TestReal(t *testing.T) {
+	s := State{
+		User: reacto.Ref(&User{
+			Name:  "User",
+			Age:   10,
+			Phone: "123",
+		}),
+		Card: reacto.Ref(&Card{
+			Number: reacto.Ref("123123"),
+			Pin:    "456",
+		}),
+	}
+
+	s.CardTitle = reacto.Computed(func() string {
+		return s.User.Value().Name + s.Card.Value().Number.Value()
+	})
+
+	reacto.Watch("card data changed", func() {
+		t.Log("Card changed", s.Card.Value())
+	})
+
+	reacto.Watch("user data changed", func() {
+		t.Log("New user data", s.User.Value())
+	})
+
+	card := s.Card.Value()
+
+	reacto.Watch("card number changed", func() {
+		t.Log("Card num changed", card.Number.Value())
+	})
+
+	reacto.Watch("CARD TITLE CHANGED", func() {
+		t.Log("CARD TITLE CHANGED", s.CardTitle.Value())
+	})
+
+	t.Log("-------------")
+
+	card.Pin = "090900909"
+	s.Card.Set(card)
+
+	t.Log("-------------")
+
+	card.Number.Set("0999009990")
+}
+
+func TestPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("expected panic")
+		}
+	}()
+
+	a := reacto.Ref(1)
+	reacto.Watch("a", func() {
+		a.Set(3)
+	})
 }
